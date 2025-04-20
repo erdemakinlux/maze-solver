518c5091de436147cc32bc78c4fd63da4fa3e23c
erdemakinlux <erdem.akin.lux>
2025-04-20 14:25:39 +0200

    end of L3

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-from tkinter import Tk, BOTH, Canvas
-from tkinter import ttk
-
-class Window():
-    def __init__(self, width, height):
-        self.width = width
-        self.heigth = height
-        root = Tk()
-        root.title("Maze Solver")
-
-        C = Canvas(root, bg="blue", height=250, width=300)
-
-        C.pack()
-        root.mainloop()
+from window import Window
 
 
 def main():
-    wd = Window(100, 100)
+    win = Window(800, 600)
+    win.wait_for_close()
     print("Maze Solver")
 
 if __name__ == "__main__":
